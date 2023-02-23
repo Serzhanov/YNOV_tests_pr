06f4c785af5a68e44737910e75949e369158794f
Serzhanov Nurbek <ns490367@gmail.com>
2023-02-23 17:52:43 +0100

    delete error management

--- app/people.py
+++ app/people.py
@@ -5,7 +5,6 @@ from config import db
 from models import Person, people_schema, person_schema
 
 
-
 def read_all():
     people = Person.query.all()
     return people_schema.dump(people)
@@ -18,6 +17,7 @@ def create(person):
         new_person = person_schema.load(person, session=db.session)
         db.session.add(new_person)
         db.session.commit()
+        #db.session.close()
         return person_schema.dump(new_person), 201
     else:
         abort(406, f"Person with last name {lname} already exists")
@@ -38,16 +38,28 @@ def update(lname, person):
         existing_person.fname = update_person.fname
         db.session.merge(existing_person)
         db.session.commit()
+        #db.session.close()
         return person_schema.dump(existing_person), 201
     else:
         abort(404, f"Person with last name {lname} not found")
 
 def delete(lname):
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    #  with app.app_context():
+    #     person = Person.query.get(person_id)
+    #     if person is None:
+    #         return jsonify({'error': 'Person not found'}), 404
+    #     db.session.delete(person)
+    #     db.session.commit()
+    #     db.session.close()  # close the database session
+    #     return jsonify({'success': 'Person deleted'})
 
+    
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    print(person_schema.dump(existing_person).get('lname'))
     if existing_person:
         db.session.delete(existing_person)
         db.session.commit()
-        return make_response(f"{lname} successfully deleted", 200)
+        #db.session.close()
+        return make_response(f"{lname} successfully deleted", 204)
     else:
         abort(404, f"Person with last name {lname} not found")
