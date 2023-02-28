25f1b5a9965da8dc92007613a6fc566df023973d
Serzhanov Nurbek <ns490367@gmail.com>
2023-02-28 22:32:50 +0100

    added tests via API

--- app/people.py
+++ app/people.py
@@ -1,6 +1,6 @@
 # people.py
-from flask import abort,make_response
-
+from flask import abort, make_response
+import sqlite3
 from config import db
 from models import Person, people_schema, person_schema
 
@@ -8,6 +8,7 @@ from models import Person, people_schema, person_schema
 def read_all():
     people = Person.query.all()
     return people_schema.dump(people)
+
 
 def create(person):
     lname = person.get("lname")
@@ -17,10 +18,11 @@ def create(person):
         new_person = person_schema.load(person, session=db.session)
         db.session.add(new_person)
         db.session.commit()
-        #db.session.close()
+
         return person_schema.dump(new_person), 201
     else:
         abort(406, f"Person with last name {lname} already exists")
+
 
 def read_one(lname):
     person = Person.query.filter(Person.lname == lname).one_or_none()
@@ -30,6 +32,7 @@ def read_one(lname):
     else:
         abort(404, f"Person with last name {lname} not found")
 
+
 def update(lname, person):
     existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
@@ -38,28 +41,26 @@ def update(lname, person):
         existing_person.fname = update_person.fname
         db.session.merge(existing_person)
         db.session.commit()
-        #db.session.close()
+
         return person_schema.dump(existing_person), 201
     else:
         abort(404, f"Person with last name {lname} not found")
 
-def delete(lname):
-    #  with app.app_context():
-    #     person = Person.query.get(person_id)
-    #     if person is None:
-    #         return jsonify({'error': 'Person not found'}), 404
-    #     db.session.delete(person)
-    #     db.session.commit()
-    #     db.session.close()  # close the database session
-    #     return jsonify({'success': 'Person deleted'})
 
-    
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
-    print(person_schema.dump(existing_person).get('lname'))
-    if existing_person:
-        db.session.delete(existing_person)
-        db.session.commit()
-        #db.session.close()
-        return make_response(f"{lname} successfully deleted", 204)
-    else:
-        abort(404, f"Person with last name {lname} not found")
+def delete(lname):
+    try:
+        existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+        if existing_person:
+            db.session.delete(existing_person)
+            db.session.commit()
+
+            return make_response(f"{lname} successfully deleted", 204)
+        else:
+            abort(404, f"Person with last name {lname} not found")
+    except:
+        db2 = sqlite3.connect("people.db")
+        c = db2.cursor()
+        x = c.execute("SELECT lname FROM person")
+        c.execute("DELETE FROM PERSON WHERE lname=?", (lname,))
+        db2.commit()
